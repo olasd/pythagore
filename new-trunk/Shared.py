11240f40669bf18de885718ca9a067601f895413
Nicolas Dandrimont <Nicolas.Dandrimont@crans.org>
2008-08-25 16:43:18 +0000

    * Add copyright notice to Shared.py
    git-svn-id: http://svn.teepi.net/svn/bots@74 e4fd86d8-5dfe-4472-a078-6375bbf5fb62

--- new-trunk/Shared.py
+++ new-trunk/Shared.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Pythagore ("2.0")
+# A Python IRC Bot
+#
+# Shared.py : shared utility classes and functions
+#
+# Copyright (C) 2008 Guillaume Seguin <guillaume@segu.in>
+# Copyright (C) 2008 Nicolas Danrimont <Nicolas.Dandrimont@crans.org>
+#
+# This file is part of Pythagore.
+#
+# Pythagore is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# Pythagore is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# of FITNESS FOR ANY PARTICULAR PURPOSE. See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pythagore; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+
 def to_unicode (txt, pray_enc_is="ISO8859-15"):
     if isinstance(txt, unicode):
         return txt
